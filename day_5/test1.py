c5d2c1544e8e198d05758b0d330c37b5700ef0e0
Likhith-Coforge <Likhith.Cholleti@coforge.com>
2025-07-16 12:51:51 +0530

    test1

---
+++ day_5/test1.py
@@ -0,0 +1,20 @@
+# steps: 
+
+# 1) all of you get username
+# 2) input should be in a lowercase
+# 3) convert it into the upper case without using the string .upper() method
+
+
+user_name = input('Enter your name: ').lower()
+
+resulting_name = ""
+
+resulting_name+=chr(ord(user_name[0])-32)
+resulting_name+=chr(ord(user_name[1])-32)
+resulting_name+=chr(ord(user_name[2])-32)
+resulting_name+=chr(ord(user_name[3])-32)
+resulting_name+=chr(ord(user_name[4])-32)
+resulting_name+=chr(ord(user_name[5])-32)
+resulting_name+=chr(ord(user_name[6])-32)
+
+print(resulting_name)
