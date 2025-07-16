421fe88a9c25436ba383517b31abdafca9fa13d7
Likhith-Coforge <Likhith.Cholleti@coforge.com>
2025-07-16 10:58:34 +0530

    class activities

---
+++ day_5/class_activities.py
@@ -0,0 +1,13 @@
+activity_2
+print("Hello, world!")
+print("Hello,\nworld!")
+
+print("16","07","2025",sep="-")
+print("Loading",end="... ")
+print("Done")
+
+name = "Anand"
+marks = 95
+print(f"Name: {name} Marks: {marks}")
+
+print("5","4","3","2","1","Liftoff!",sep="-->")
